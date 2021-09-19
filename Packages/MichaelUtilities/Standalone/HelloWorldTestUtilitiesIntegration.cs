7fa4d2dfd305120ee77eeeb82bff6e8d1c08d6e0
Michael <michael.b.rawls@gmail.com>
2021-09-18 23:23:05 -0500

    Added HelloWorld to test Utilities when added to other projects. Added rootSpaceName of project to Assembly Definitions.

---
+++ Packages/MichaelUtilities/Standalone/HelloWorldTestUtilitiesIntegration.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MichaelUtilities.Standalone
+{
+    class HelloWorld
+    {
+        public void HelloWorldToConsole()
+        {
+            Console.WriteLine("Hello World, I'm calling from the Utilities library!");
+        }
+    }
+}
